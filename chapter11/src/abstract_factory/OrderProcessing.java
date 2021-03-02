1cb5093d95bc2e9ebb6a9830904cac96d77206ee
boring-km <kms0644804@naver.com>
2021-03-02 17:09:53 +0900

    [master] DIP 반영하여 소스코드 수정하자

--- chapter11/src/abstract_factory/OrderProcessing.java
+++ chapter11/src/abstract_factory/OrderProcessing.java
@@ -2,7 +2,13 @@ package abstract_factory;
 
 public class OrderProcessing {
 
-    public LineItem execute(ItemFactory lineItemFactory) {
-        return lineItemFactory.makeLineItem(); // 객체 생성 시점은 애플리케이션이 결정한다.
+    private final ItemFactory itemFactory;
+
+    public OrderProcessing(ItemFactory itemFactory) {   // DIP 반영
+        this.itemFactory = itemFactory;
+    }
+
+    public LineItem execute() {
+        return itemFactory.makeLineItem(); // 객체 생성 시점은 애플리케이션이 결정한다.
     }
 }
--- chapter11/test/abstract_factory/MainTest.java
+++ chapter11/test/abstract_factory/MainTest.java
@@ -10,10 +10,10 @@ class MainTest {
     void name() {
         // given
         ItemFactory factory = new LineItemFactory();    // 객체를 생성할 Factory 클래스에 대한 의존성
-        OrderProcessing orderProcessing = new OrderProcessing();    // 실행을 위한 의존성
+        OrderProcessing orderProcessing = new OrderProcessing(factory);    // 실행을 위한 의존성
 
         // when
-        LineItem lineItem = orderProcessing.execute(factory);   // 실행
+        LineItem lineItem = orderProcessing.execute();   // 실행
 
         // then
         LineItem expected = new LineItem("고양이");
